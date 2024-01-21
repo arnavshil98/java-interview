b41a38fa9f900403d606393ff7a33917b82b60ba
arnav <arnav.shil@decipherzone.com>
2024-01-21 16:35:33 +0530

    sync block

--- src/main/java/org/example/Multithreading/TestSynchronizedBlock1.java
+++ src/main/java/org/example/Multithreading/TestSynchronizedBlock1.java
@@ -2,14 +2,14 @@ package org.example.Multithreading;
 class Table
 {
    synchronized void  printTable(int n){
-//        synchronized(this){//synchronized block
+        synchronized(this){//synchronized block
             for(int i=1;i<=5;i++){
                 System.out.println(n*i);
                 try{
                     Thread.sleep(400);
                 }catch(InterruptedException e){System.out.println(e);}
             }
-//        }
+        }
     }//end of the method
 }
 
